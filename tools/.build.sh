791a472881cab3d5c219709b876b47ba4c0307a6
Joel16 <sauskethunderkid@gmail.com>
2018-03-17 23:32:38 -0500

    Remove .travis.yml from build

--- tools/.build.sh
+++ tools/.build.sh
@@ -4,5 +4,5 @@ set -x
 
 cmake -G "Unix Makefiles"
 make clean && make
-rm .gitattributes CMakeCache.txt cmake_install.cmake CMakeLists.txt eboot.bin LICENSE README.md Makefile VitaBackup VitaBackup.velf VitaBackup.vpk_param.sfo
+rm .gitattributes .travis.yml CMakeCache.txt cmake_install.cmake CMakeLists.txt eboot.bin LICENSE README.md Makefile VitaBackup VitaBackup.velf VitaBackup.vpk_param.sfo
 rm -rf CMakeFiles include res sce_sys source tools 
